ba91511afc14889ed7427085f7737bec1eb78568
Mateusz Zając <deejay.zajac@gmail.com>
2012-07-17 22:33:22 +0200

    1.1 Added Permisions support for PermisionsEX

--- src/com/zajacmp3/indestructibletools/IndestructibleTools.java
+++ src/com/zajacmp3/indestructibletools/IndestructibleTools.java
@@ -1,6 +1,7 @@
 package com.zajacmp3.indestructibletools;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Server;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -11,12 +12,18 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import ru.tehkode.permissions.PermissionManager;
+import ru.tehkode.permissions.bukkit.PermissionsEx;
+
 public class IndestructibleTools extends JavaPlugin implements Listener{
+	public static Server server;
+	public static PermissionManager Permissions;
 	public void onDisable(){
 		
 	}
 	public void onEnable(){
 		load();
+		setupPermissions();
 		PluginDescriptionFile description = this.getDescription();
 		System.out.println("IndestructibleTools "+description.getVersion()+" is launching");
 		EventListener(this);
@@ -33,23 +40,53 @@ public class IndestructibleTools extends JavaPlugin implements Listener{
 	@EventHandler(priority = EventPriority.NORMAL)
 	public void itemUsage(final PlayerInteractEvent event){
 		Player player = event.getPlayer();
-		player.getItemInHand().setDurability((short) 0);
+		if(isAllowed(player,"tools")){
+			player.getItemInHand().setDurability((short) 0);
+		}
 	}
 	@EventHandler(priority = EventPriority.NORMAL)
 	public void playerDamaged(final EntityDamageByEntityEvent event){
 		if(event.getEntity() instanceof Player){
 			Player player = (Player)event.getEntity();
-			if(player.getInventory().getHelmet()!=null){
-				player.getInventory().getHelmet().setDurability((short) 0);
+			if(isAllowed(player,"armor")){
+				if(player.getInventory().getHelmet()!=null){
+					player.getInventory().getHelmet().setDurability((short) 0);
+				}
+				if(player.getInventory().getBoots()!=null){
+					player.getInventory().getBoots().setDurability((short) 0);
+				}
+				if(player.getInventory().getLeggings()!=null){
+					player.getInventory().getLeggings().setDurability((short) 0);
+				}
+				if(player.getInventory().getChestplate()!=null){
+					player.getInventory().getChestplate().setDurability((short) 0);
+				}
 			}
-			if(player.getInventory().getBoots()!=null){
-				player.getInventory().getBoots().setDurability((short) 0);
+		}
+	}
+	private void setupPermissions(){
+		if (IndestructibleTools.Permissions == null){
+			if(getServer().getPluginManager().isPluginEnabled("PermissionsEx")){
+				IndestructibleTools.Permissions = PermissionsEx.getPermissionManager();
 			}
-			if(player.getInventory().getLeggings()!=null){
-				player.getInventory().getLeggings().setDurability((short) 0);
+			else{
+				System.out.println("Permission system not detected, defaulting to OP");
 			}
-			if(player.getInventory().getChestplate()!=null){
-				player.getInventory().getChestplate().setDurability((short) 0);
+		}
+	}
+	public static boolean isAllowed( Player player, String string ){
+		if( Permissions == null ){
+			return true;
+		}
+		if( Permissions.has(player, "indestructibletools.armor")){
+			return true;
+		}
+		else{
+			if( player.isOp() == true){
+					return true;
+			}
+			else{
+				return false;
 			}
 		}
 	}
